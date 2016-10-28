d1b42892a40d41a4473f370ed4f201cc53242756
Rickon Stark <Rickon@iMac-de-Rickon.local>
2016-10-28 13:12:59 -0500

    Post css configuration

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@ var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var nested = require('postcss-nested');
 var atImport = require("postcss-import");
-
+var concat = require('gulp-concat');
+var cssnano = require('cssnano');
+var notify = require('gulp-notify');
 var gulp = require('gulp');
+
 
 gulp.task('css', function () {
     var processors = [
-        cssnext({browsers: ['last 1 version']}),
+        cssnext({browsers: ['last 1 version'], warnForDuplicates: false }),
         nested(),
         atImport(),
+        cssnano()
     ];
-    return gulp.src('./_cssnext/main.css')
+    return gulp.src('./_cssnext/*.css')
+        .pipe(concat('main.min.css'))
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(notify({ message: 'Tarea Post css completa' }));
 });
 
-gulp.task('default', ['css']);
+gulp.task('watch', function () {
+    gulp.watch('_cssnext/*.css', ['css']);
+});
 
+gulp.task('default', ['watch']);
